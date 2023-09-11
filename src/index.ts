14d1793a769a4f31293fcebc57eb955756629d74
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2023-09-11 22:55:54 +0530

    bun init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
 import { Elysia } from "elysia";
+const superheroes = require("superheroes");
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+
+app.get("/", () => "Hello Elysia");
+app.get("/welcome", () => ({ message: "It is fast." }));
+app.get("/random", () => ({ name: superheroes.random() }));
+app.get("/all", () => ({ names: superheroes.all }));
+app.get("/search/:name", (req) => {
+  const name = req.params.name;
+  const superheroesNames = superheroes.all;
+
+  for (let i = 0; i < superheroesNames.length; i++) {
+    const element = superheroesNames[i];
+    if (element.includes(name)) {
+      return {
+        name: element,
+      };
+    }
+  }
+
+  return {
+    message: "No match found",
+  };
+});
+app.post("/my-hero", (context: any) => {
+  const name = context.body.name;
+  const random = superheroes.random();
+
+  return {
+    message: `Hey! ${name}, Your lucky partner is ${random}`,
+  };
+});
+app.listen(process.env.PORT ?? 3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at server: ${app.server?.hostname}:${app.server?.port}`
 );
